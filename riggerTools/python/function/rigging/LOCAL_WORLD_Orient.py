e8c125f9bfcc1ae7a3da5423859b7d1fbda703bf
narongtum <narongtum_sri@jumbojumps.com>
2025-05-30 18:04:05 +0700

    update

--- riggerTools/python/function/rigging/LOCAL_WORLD_Orient.py
+++ riggerTools/python/function/rigging/LOCAL_WORLD_Orient.py
@@ -1,4 +1,8 @@
 import maya.cmds as mc
+from function.framework.reloadWrapper import reloadWrapper as reload
+from function.rigging.autoRig.base import core
+reload(core)
+
 
 # orient LOCAL WORLD 
 # select controller
@@ -10,19 +14,25 @@ def localWorldOrient( worldCtrl = 'placement_ctrl' ):
 		mc.pickWalk(d='up')
 		upSel = mc.pickWalk(d='up')
 		upGrp = upSel[0]
-		preName = sel[i].split('_')
-		name = preName[0]
+		name = core.check_name_style(sel[i])[0]
+		# preName = sel[i].split('_')
+		# name = preName[0]
 		
 		grp = name + 'Zro_grp'
 		wor = name + 'Wor_grp'
 		loc = name + 'Loc_grp'
 		rev = name + '_rev'
+
 		pov = name + '_ctrl'
+		povShape = core.shapeName(pov)
+
+		
+
 		pConAttrLoc = grp + '_orientConstraint1' + '.' + loc + 'W0'
 		pConAttrWor = grp + '_orientConstraint1' + '.' + wor + 'W1'
 
-		# adding attr for selected controller
-		mc.addAttr( pov , longName = 'localWorld', attributeType = 'short', defaultValue = 0 , max = 1, min = 0,keyable=True)
+		#... adding attr for selected controller
+		mc.addAttr( povShape , longName = 'localWorld', attributeType = 'short', defaultValue = 0 , max = 1, min = 0,keyable=True)
 		
 		mc.group(em=1,n=wor)
 		mc.group(em=1,n=loc)
@@ -34,11 +44,11 @@ def localWorldOrient( worldCtrl = 'placement_ctrl' ):
 		mc.delete(loc + '_parentConstraint1')
 		
 		mc.parent(wor,loc,upGrp)
-		# World to wor
+		#... World to wor
 		mc.orientConstraint( worldCtrl, wor,w=1,mo=1 )
-		# Parent to GRP
+		#... Parent to GRP
 		mc.orientConstraint(loc,wor,grp,w=0,mo=1)
-		# Connect povCtrl to rev to pCon
-		mc.connectAttr(pov + '.localWorld', rev + '.inputX')
+		#... Connect povCtrl to rev to pCon
+		mc.connectAttr(povShape + '.localWorld', rev + '.inputX')
 		mc.connectAttr(rev + '.outputX', pConAttrLoc)
 		mc.connectAttr(rev + '.inputX', pConAttrWor)

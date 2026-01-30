f5c0d0828bf69f3da449f311d6b4f04cc76ef093
narongtum <nomangarden@gmail.com>
2026-01-30 13:50:31 +0700

    Your commit message here

--- riggerTools/python/function/rigging/util/generic_maya_dict.py
+++ riggerTools/python/function/rigging/util/generic_maya_dict.py
@@ -429,6 +429,27 @@ standard_jnt_dict = {
 }
 
 
+ue_jnt_dict = {
+    'root': ['root', 'weapon_l', 'weapon_r'],
+    'spine': ['pelvis', 'spine_01', 'spine_02', 'spine_03'],
+    'head': ['neck_01', 'head', 'eye_l', 'eye_r'],
+    'L_arm': ['clavicle_l', 'upperarm_l', 'lowerarm_l', 'hand_l',
+              'thumb_01_l', 'thumb_02_l', 'thumb_03_l',
+              'index_01_l', 'index_02_l', 'index_03_l',
+              'middle_01_l', 'middle_02_l', 'middle_03_l',
+              'ring_01_l', 'ring_02_l', 'ring_03_l',
+              'pinky_01_l', 'pinky_02_l', 'pinky_03_l'],
+    'R_arm': ['clavicle_r', 'upperarm_r', 'lowerarm_r', 'hand_r',
+              'thumb_01_r', 'thumb_02_r', 'thumb_03_r',
+              'index_01_r', 'index_02_r', 'index_03_r',
+              'middle_01_r', 'middle_02_r', 'middle_03_r',
+              'ring_01_r', 'ring_02_r', 'ring_03_r',
+              'pinky_01_r', 'pinky_02_r', 'pinky_03_r'],
+    'L_leg': ['thigh_l', 'calf_l', 'foot_l', 'ball_l'],
+    'R_leg': ['thigh_r', 'calf_r', 'foot_r', 'ball_r']
+}
+
+
 
 
 
